fa00568eba6b668bccb636aeb281b01357cc7b2f
Anurag Nishad <124987038+AnuragNishad12@users.noreply.github.com>
2024-07-10 14:12:01 +0530

    Authentication

--- app/src/main/java/com/example/juttela/FrontPage/Sign_Up_Page.java
+++ app/src/main/java/com/example/juttela/FrontPage/Sign_Up_Page.java
@@ -3,8 +3,10 @@ package com.example.juttela.FrontPage;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,11 +15,24 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.juttela.MainActivity;
 import com.example.juttela.R;
 import com.example.juttela.databinding.ActivitySignUpPageBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.abt.FirebaseABTesting;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Sign_Up_Page extends AppCompatActivity {
 
 
     ActivitySignUpPageBinding binding;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +49,52 @@ public class Sign_Up_Page extends AppCompatActivity {
         }
     });
 
+    binding.signButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            binding.progressBar.setVisibility(View.VISIBLE);
+
+            String name = binding.fullname.getText().toString();
+            String email =  binding.email.getText().toString();
+            String password = binding.password.getText().toString();
+            if (name.isEmpty() && email.isEmpty() && password.isEmpty()){
+                Toast.makeText(Sign_Up_Page.this, "Fill All the Field", Toast.LENGTH_SHORT).show();
+            }else {
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            binding.progressBar.setVisibility(View.GONE);
+                            FirebaseUser user = auth.getCurrentUser();
+
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("name", name);
+                            map.put("email", email);
 
 
+                            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
+                            databaseReference.child("users").child(user.getUid()).setValue(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(Sign_Up_Page.this, "User Added Succefully", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Sign_Up_Page.this,MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(Sign_Up_Page.this, "Error" + e, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }else{
+                            Toast.makeText(Sign_Up_Page.this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+    });
     }
 }
