b172fadbda6bea1cb9d6504e910f0f744c64ed60
Anurag Nishad <124987038+AnuragNishad12@users.noreply.github.com>
2024-07-26 15:24:22 +0530

    Fecth Data for Setting activity_setting.xml

--- app/src/main/java/com/example/juttela/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/juttela/Fragment/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -20,6 +21,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class ProfileFragment extends Fragment {
 
@@ -44,7 +51,9 @@ public class ProfileFragment extends Fragment {
         database.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()){
+                if (dataSnapshot.exists()) {
+                    List<String> list = new ArrayList<>();
+
                     String name = dataSnapshot.child("name").getValue(String.class);
                     String age = dataSnapshot.child("age").getValue(String.class);
                     String country = dataSnapshot.child("country").getValue(String.class);
@@ -59,7 +68,29 @@ public class ProfileFragment extends Fragment {
                     Glide.with(requireContext())
                             .load(imageUri)
                             .into(binding.profileImage);
+
+                    DataSnapshot additionalDataSnapshot = dataSnapshot.child("additionalData");
+                    if (additionalDataSnapshot.exists()) {
+                        for (DataSnapshot languageSnapshot : additionalDataSnapshot.getChildren()) {
+                            String language = languageSnapshot.getValue(String.class);
+                            if (language != null) {
+                                list.add(language);
+                            }
+                        }
+
+                        // Display languages
+                        if (!list.isEmpty()) {
+                            String languagesString =String.join(", ", list);
+                            binding.profileLearningLan.setText(languagesString);
+                        } else {
+                            binding.profileLearningLan.setVisibility(View.GONE);
+                        }
+                    } else {
+                        binding.profileLearningLan.setVisibility(View.GONE);
+                    }
+
                 }
+
             }
 
             @Override
--- app/src/main/java/com/example/juttela/Setting_Activity.java
+++ app/src/main/java/com/example/juttela/Setting_Activity.java
@@ -1,25 +1,92 @@
 package com.example.juttela;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
 import com.example.juttela.databinding.ActivitySettingBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Setting_Activity extends AppCompatActivity {
 
 
     ActivitySettingBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         binding = ActivitySettingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference database =  FirebaseDatabase.getInstance().getReference().child("FinalUser").child(uid);
+
+
+        database.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+
+                    String name = dataSnapshot.child("name").getValue(String.class);
+                    String age = dataSnapshot.child("age").getValue(String.class);
+                    String country = dataSnapshot.child("country").getValue(String.class);
+                    String gender = dataSnapshot.child("gender").getValue(String.class);
+                    binding.editNameText.setText(name);
+                    binding.editAge.setText(age);
+                    binding.editCountry.setText(country);
+                    binding.editGender.setText(gender);
+                    String imageUri = dataSnapshot.child("imageUri").getValue(String.class);
+//                    binding.profileLan.setText(selectedCountry);
+                    Glide.with(Setting_Activity.this)
+                            .load(imageUri)
+                            .into(binding.profileImageEdit);
+
+//                    DataSnapshot additionalDataSnapshot = dataSnapshot.child("additionalData");
+//                    if (additionalDataSnapshot.exists()) {
+//                        for (DataSnapshot languageSnapshot : additionalDataSnapshot.getChildren()) {
+//                            String language = languageSnapshot.getValue(String.class);
+//                            if (language != null) {
+//                                list.add(language);
+//                            }
+//                        }
+//
+////                         Display languages
+//                        if (!list.isEmpty()) {
+//                            String languagesString =String.join(", ", list);
+//                            binding.profileLearningLan.setText(languagesString);
+//                        } else {
+//                            binding.profileLearningLan.setVisibility(View.GONE);
+//                        }
+//                    } else {
+//                        binding.profileLearningLan.setVisibility(View.GONE);
+//                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
 
     }
 }
